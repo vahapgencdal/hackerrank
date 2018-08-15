8a5d05de82434aeb6505f4431c033b0f942740d3
vahapgencdal <avahap19@gmail.com>
2018-08-15 14:59:07 +0300

    solve me first question

---
+++ easy/solve-me-first/index.py
@@ -0,0 +1,7 @@
+
+from math import sqrt
+
+def sum_of_two_number(first_number, second_number):
+    return first_number+second_number
+
+print(sum_of_two_number(5,12))
---
+++ easy/solve-me-first/readme.md
@@ -0,0 +1,3 @@
+##Solve Me First  
+
+**[Question link](https://www.hackerrank.com/challenges/solve-me-first/problem)**
